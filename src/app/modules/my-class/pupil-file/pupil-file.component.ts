5269785656ca1b5fea46e4da8f73bca43704f63d
Antoine <antoinedesch62@gmail.com>
2022-02-02 19:03:55 +0100

    skill grid

--- src/app/modules/my-class/pupil-file/pupil-file.component.ts
+++ src/app/modules/my-class/pupil-file/pupil-file.component.ts
@@ -1,6 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {SkillHttpServiceService} from "../../../service/skill-http-service.service";
 import {Skill} from "../../../models/skill";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {Subject} from "../../../models/enums/Subject";
+
+export interface SubjectTableData {
+  subject: string;
+  skills: Skill[]
+}
 
 @Component({
   selector: 'app-pupil-file',
@@ -9,15 +18,41 @@ import {Skill} from "../../../models/skill";
 })
 export class PupilFileComponent implements OnInit {
 
+  displayedColumns: string[] = ['subject', 'skills','linkedSkills'];
+  dataSource: MatTableDataSource<SubjectTableData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private skillHttpService: SkillHttpServiceService) {
   }
 
   skills: Skill[];
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   ngOnInit(): void {
     this.skillHttpService.getAllSkills().subscribe((skills) => {
       this.skills = skills;
+      let datas = Object.values(Subject).map(subject => this.createSkillTableData(subject));
+      this.dataSource = new MatTableDataSource(datas);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
   }
+
+  createSkillTableData(subject: string): SubjectTableData {
+    return {
+      subject: subject,
+      skills: this.skills.filter((skill) => skill.subject.valueOf() === subject)
+    }
+  };
 
 }
