a10b7925d9c9a1ebb8b024825ea0bfc7ed4f4596
antoine <antoinedesch62@gmail.com>
2022-03-31 22:17:56 +0200

    lsu module

---
+++ src/app/modules/lsu/lsu-edit/lsu-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lsu-edit',
+  templateUrl: './lsu-edit.component.html',
+  styleUrls: ['./lsu-edit.component.scss']
+})
+export class LsuEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/lsu/lsu.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LsuEditComponent } from './lsu-edit/lsu-edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    LsuEditComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LsuModule { }
